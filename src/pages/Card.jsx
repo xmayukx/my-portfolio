11ebed11a76ca0c8e806c6dcf78f87c276b65463
Mayukh Hazari <89103181+xmayukx@users.noreply.github.com>
2023-03-17 18:18:57 +0530

    Fix errors 2

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -4,24 +4,24 @@ import ExternalLink from './icons/ExternalLink';
 import GitHub from "./icons/Github"
 
 export default function Card(props) {
-
+    const {title, decription, repolink, exlink} = props;
     return (
         <div>
             <div className="relative mb-5 lg:mb-0 p-7 lg:h-[20rem] h-[16rem]  bg-gray-800 rounded-lg shadow hover:bg-transparent dark:bg-gray-800 dark:border-gray-700 inco w-4.5/5 hover:border hover:border-gray-700">
                 <a>
                     <h5 className="mb-2 lg:text-2xl text-lg font-semibold tracking-tight text-yellow-300">
-                        {props.title}
+                        {title}
                     </h5>
                 </a>
                 <p className="mb-3 font-normal lg:text-base text-sm  text-gray-100 dark:text-gray-400">
-                    {props.description}
+                    {description}
                 </p>
                 <div className="flex absolute bottom-5 gap-11 ">
-                    <Link href={props.repoLink}>
+                    <Link href={repoLink}>
                         <GitHub />
                     </Link>
                     {
-                        props.exlink && <Link href={props.exlink}>
+                        exlink && <Link href={exlink}>
                             <ExternalLink />
                         </Link>
                     }
