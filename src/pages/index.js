a92b71c967278d580a70d25c4a2e762d3e3d8a3b
Mayukh Hazari <89103181+xmayukx@users.noreply.github.com>
2023-03-20 18:04:47 +0530

    Add title to website

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import Navbar from "./Navbar";
 import ReactDOM, { useEffect, useState } from "react";
 import Hometxt from "./Hometxt";
 import Dock from "./Dock";
+import Head from "next/head";
 
 export default function Home() {
 
   return (
+    
     <div>
+    <Head>
+      <title>Mayukh Hazari</title>
+    </Head>
       <Navbar />
       <Dock/>
       <Hometxt />
