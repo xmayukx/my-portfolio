bc7d0a2d6fa60b1c48cbf8b8b56648ee5fe175a2
Mayukh Hazari <89103181+xmayukx@users.noreply.github.com>
2023-05-15 15:04:42 +0530

    Add social links to mobile view

--- src/pages/Dock.jsx
+++ src/pages/Dock.jsx
@@ -5,24 +5,48 @@ import Linkedin from "./icons/LinkedIn";
 import Twitter from "./icons/Twitter";
 
 export default function Dock() {
-    return (
-        <>
-            <div  className="lg:visible invisible">
-                <ul className="grid grid-cols-1 gap-8 pl-8 top-[15rem] fixed left-0 ">
-                    <Link href="https://github.com/xmayukx" className="hover:text-yellow-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200" >
-                        <li><GitHub /></li>
-                    </Link>
-                    <Link href="https://twitter.com/xmayuk_hx" className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200">
-                        <li><Twitter /></li>
-                    </Link>
-                    <Link href="https://www.instagram.com/xmayuk_hx/" className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200">
-                        <li><Instagram /></li>
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/mayukh-hazari-212276220" className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200">
-                        <li><Linkedin /></li>
-                    </Link>
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="lg:visible invisible">
+        <ul className="grid grid-cols-1 gap-8 pl-8 top-[15rem] fixed left-0 ">
+          <Link
+            href="https://github.com/xmayukx"
+            className="hover:text-yellow-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200"
+            target="_blank"
+          >
+            <li>
+              <GitHub />
+            </li>
+          </Link>
+          <Link
+            href="https://twitter.com/xmayuk_hx"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200"
+            target="_blank"
+          >
+            <li>
+              <Twitter />
+            </li>
+          </Link>
+          <Link
+            href="https://www.instagram.com/xmayuk_hx/"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200"
+            target="_blank"
+          >
+            <li>
+              <Instagram />
+            </li>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/mayukh-hazari-212276220"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200"
+            target="_blank"
+          >
+            <li>
+              <Linkedin />
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -4,34 +4,34 @@ import GitHub from "./icons/Github";
 import Instagram from "./icons/Instagram";
 import Linkedin from "./icons/LinkedIn";
 import Twitter from "./icons/Twitter";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 export default function Intro() {
-
   return (
     <div className="flex flex-col gap-5 mt-[5rem]">
       <div className="flex flex-col">
         <span className="robo text-yellow-300">Hi, my name is</span>
-        <span className="text-slate-200 mt-[1px] montse text-4xl lg:text-6xl font-extrabold">Mayukh Hazari</span>
+        <span className="text-slate-200 mt-[1px] montse text-4xl lg:text-6xl font-extrabold">
+          Mayukh Hazari
+        </span>
         <div className="flex text-slate-400 mt-[1px] montse text-4xl lg:text-6xl font-extrabold">
           <span className="mr-[10px]">I&apos;m a</span>
           <span>
             <Typewriter
               options={{
-                strings: ['developer', 'reader', 'writer'],
+                strings: ["developer", "reader", "writer"],
                 autoStart: true,
                 loop: true,
-                changeDeleteSpeed: "natural"
+                changeDeleteSpeed: "natural",
               }}
             />
           </span>
         </div>
-
-
       </div>
 
       <span className="text-slate-50 merri font-thin lg:mr-[20rem] mr-[0rem]">
-        Transforming ideas into seamless digital experiences through full-stack development and occasional design expertise.
+        Transforming ideas into seamless digital experiences through full-stack
+        development and occasional design expertise.
       </span>
 
       <div className="flex flex-row gap-5 lg:invisible">
@@ -40,17 +40,20 @@ export default function Intro() {
             <GitHub />
           </div>
         </Link>
-        <Link href="#" target="_blank">
+        <Link
+          href="https://www.linkedin.com/in/mayukh-hazari-212276220"
+          target="_blank"
+        >
           <div className="w-7 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Linkedin />
           </div>
         </Link>
-        <Link href="#" target="_blank">
+        <Link href="https://twitter.com/xmayuk_hx" target="_blank">
           <div className="w-7 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-            <Twitter/>
+            <Twitter />
           </div>
         </Link>
-        <Link href="#" target="_blank">
+        <Link href="https://www.instagram.com/xmayuk_hx/" target="_blank">
           <div className="w-7 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Instagram />
           </div>
